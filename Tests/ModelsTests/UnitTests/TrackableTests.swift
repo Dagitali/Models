6f5a88366789c712ec3a63438030b2ca7d995991
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2025-02-01 13:39:30 -0500

    refactor(protocol): Rename DateTrackableTest.swift
    ...to TrackableTest.swift.
    Replace DateTrackable w/ Trackable in comments.
    Refine unit test.
    Refactor unit test.

--- Tests/ModelsTests/UnitTests/TrackableTests.swift
+++ Tests/ModelsTests/UnitTests/TrackableTests.swift
@@ -1,5 +1,5 @@
 //
-//  DateTrackableTests.swift
+//  TrackableTests.swift
 //  Models
 //
 //  Copyright © 2025 Dagitali LLC. All rights reserved.
@@ -9,7 +9,7 @@
  See the LICENSE.txt file for this package’s licensing information.
 
  Abstract:
- A test suite to validate the functionality of the `DateTrackable` protocol.
+ A test suite to validate the functionality of the `Trackable` protocol.
 */
 
 import Foundation
@@ -19,9 +19,9 @@ import Testing
 
 // MARK: - Fixtures
 
-/// A mock conforming to `DateTrackable`.
+/// A mock conforming to `Trackable`.
 @Model
-final class MockPersistentModel: DateTrackable {
+final class MockPersistentModel: Trackable {
     // MARK: Properties
 
     // Required
@@ -42,16 +42,16 @@ final class MockPersistentModel: DateTrackable {
 
 // MARK: - Tests
 
-/// A test suite to validate the functionality of `DateTrackable`extensions.
-@Suite("DateTrackable Tests")
-struct DateTrackableTests {
+/// A test suite to validate the functionality of `Trackable`extensions.
+@Suite("Trackable Tests")
+struct TrackableTests {
     // MARK: Protocol
 
     /// Tests the `update()` method.
     ///
     /// This ensures the method correctly updates.
     @Test
-    func testMockDateTrackable() {
+    func testMockTrackable() {
         // Given...
         let timestamp = MockPersistentModel(name: "John Doe")
 
@@ -82,23 +82,9 @@ struct DateTrackableTests {
         )
 
         // When...
-        mock.updatedAt = .now
-        let firstUpdateTime = mock.updatedAt
-
-        // Then...
-        #expect(
-            mock.name == "Jane Doe",
-            "name should be Jane Doe."
-        )
-        #expect(
-            mock.updatedAt > mock.createdAt,
-            "updatedAt should be more recent than createdAt after updating a property."
-        )
-
-        // When...
         Thread.sleep(forTimeInterval: 0.5)
         mock.update(forKey: \.name, to: "Jane Doe")
-        let firstUpdateTime = mock.updatedAt
+        let updatedAt1 = mock.updatedAt
 
         // Then...
         #expect(
@@ -113,7 +99,7 @@ struct DateTrackableTests {
         // When...
         Thread.sleep(forTimeInterval: 0.5)
         mock.update(forKey: \.name, to: "Mary Jane Doe")
-        let secondUpdateTime = mock.updatedAt
+        let updatedAt2 = mock.updatedAt
 
         // Then...
         #expect(
@@ -125,8 +111,27 @@ struct DateTrackableTests {
             "updatedAt should be more recent than createdAt after updating a property."
         )
         #expect(
-            secondUpdateTime > firstUpdateTime,
-            "updatedAt should be more recent than createdAt after updating a property."
+            updatedAt2 > updatedAt1,
+            "updatedAt #2 should be more recent than updatedAt #1 after updating a property."
+        )
+
+        // When...
+        Thread.sleep(forTimeInterval: 0.5)
+        mock.updatedAt = .now
+        let updatedAt3 = mock.updatedAt
+
+        // Then...
+        #expect(
+            mock.name == "Mary Jane Doe",
+            "name should be Mary Jane Doe."
+        )
+        #expect(
+            mock.updatedAt > mock.createdAt,
+            "updatedAt should be more recent than createdAt after directly updating updatedAt."
+        )
+        #expect(
+            updatedAt3 > updatedAt2,
+            "updatedAt #3 should be more recent than updatedAt #2 after updating a property."
         )
     }
 }

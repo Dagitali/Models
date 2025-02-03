00bf9e622ecde79b12b696453ffbbbb0b3f02fc6
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2025-02-02 21:20:08 -0500

    test(type): Update AgeBracketTests.swift
    Add 3 unit tests.

--- Tests/ModelsTests/UnitTests/Types/AgeBracketTests.swift
+++ Tests/ModelsTests/UnitTests/Types/AgeBracketTests.swift
@@ -21,6 +21,61 @@ import Testing
 /// A test suite to validate the functionality of the `AgeBracket` enum.
 @Suite("AgeBracket Tests")
 struct AgeBracketTests {
+    // MARK: Protocol Conformance (CaseIterable)
+
+    /// Tests conformance to the `CaseIterable` protocol.
+    ///
+    /// This ensures all of the `AgeBracket` cases are iterated.
+    @Test
+    func testAllCases() {
+        // Given...
+        let expected: [AgeBracket] = [
+            // Centers for Disease Control (CDC)
+            .infantCDC, .toddlerCDC, .preschoolerCDC, .childCDC, .teenagerCDC,
+            .youngAdultCDC, .adultCDC, .middleAgedCDC, .seniorCDC,
+
+            // U.S. Census
+            .infantCensus, .toddlerCensus, .childCensus, .youthCensus,
+            .youngAdultCensus, .middleAgedCensus, .seniorCensus, .seniorMiddleOldCensus, .seniorOldestOldCensus,
+
+            // World Health Organization (WHO)
+            .infantWHO, .earlyChildhoodWHO, .childhoodWHO, .adolescentWHO,
+            .youngAdultWHO, .middleAgedWHO, .seniorWHO
+        ]
+
+        // When...
+        let actual = AgeBracket.allCases
+
+        // Then...
+        #expect(
+            actual == expected,
+            "`AgeBracket.allCases` does not contain all expected cases."
+        )
+    }
+
+    // MARK: Protocol Conformance (Codable)
+
+    /// Tests conformance to the `Codable` protocol.
+    ///
+    /// This ensures the `AgeBracket` is both encodable and decodable.
+    @Test
+    func testCodable() throws {
+        // Given...
+        let encoder = JSONEncoder()
+        let decoder = JSONDecoder()
+        let original = AgeBracket.seniorCDC
+
+        // When...
+        let encoded = try encoder.encode(original)
+        let decoded = try decoder.decode(AgeBracket.self, from: encoded)
+
+        // Then...
+        #expect(
+            original == decoded,
+            "Encoding/decoding failed for `AgeBracket`."
+        )
+    }
+
     // MARK: Computed Properties
 
     /// Tests the `isAdult` property.
